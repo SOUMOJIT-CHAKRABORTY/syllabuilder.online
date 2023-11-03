3d70ffab4e74d0b28a061b7774ce6245a56dbee2
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-11-03 18:53:05 +0530

    getting back response from chatgpt

--- src/app/api/course/createChapters/route.ts
+++ src/app/api/course/createChapters/route.ts
@@ -1,5 +1,6 @@
 // /api/course/createChapters
 
+import { strict_output } from "@/lib/gpt";
 import { createChapterSchema } from "@/validators/course";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
@@ -16,6 +17,20 @@ export async function POST(req: Request, res: Response) {
         chapter_title: string;
       }[];
     };
+
+    let output_units: outputUnits = await strict_output(
+      "You are a AI capable of curating course content, coming up with relevant chapter titles, and finding relevant youtube videos for each chapter",
+      new Array(units.length).fill(
+        `It is your job to create a course about ${title}. The user has requested to create chapters for each of the units. Then, for each chapter, provide a detailed youtube search query that can be used to find an informative educationalvideo for each chapter. Each query should give an educational informative course in youtube.`
+      ),
+      {
+        title: "title of the unit",
+        chapters:
+          "an array of chapters, each chapter should have a youtube_search_query and a chapter_title key in the JSON object",
+      }
+    );
+    console.log(output_units);
+    return NextResponse.json(output_units);
   } catch (error) {
     if (error instanceof ZodError) {
       return new NextResponse("invalid body", { status: 400 });
