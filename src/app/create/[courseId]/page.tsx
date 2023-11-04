2f66959f7186d5417cc20776a5fbfffad257d29d
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-11-05 04:03:07 +0530

    confirm chapters page created

--- src/app/create/[courseId]/page.tsx
+++ src/app/create/[courseId]/page.tsx
@@ -1,5 +1,7 @@
+import ConfirmChapters from "@/components/ConfirmChapters";
 import { getAuthSession } from "@/lib/auth";
 import { prisma } from "@/lib/db";
+import { Info } from "lucide-react";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -29,7 +31,23 @@ const createChapters = async ({ params: { courseId } }: Props) => {
   if (!course) {
     return redirect("/create");
   }
-  return <pre>{JSON.stringify(course, null, 2)}</pre>;
+  return (
+    <div className="flex flex-col items-start max-w-xl mx-auto my-16">
+      <h5 className="text-sm uppercase text-seconday-foreground/60">
+        Course Name
+      </h5>
+      <h1 className="text-5xl font-bold">{course.name}</h1>
+
+      <div className="flex p-4 mt-5 border-none bg-secondary">
+        <Info className="flex-none w-8 h-8 mr-4 text-primary" />
+        <div>
+          We generated chapters for each of your units. Look over them and then
+          click the Button to confirm and continue
+        </div>
+      </div>
+      <ConfirmChapters course={course} />
+    </div>
+  );
 };
 
 export default createChapters;
