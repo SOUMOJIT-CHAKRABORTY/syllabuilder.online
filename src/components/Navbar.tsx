8a69abdccb0d76655a5a1e71581dcb3806bb1f15
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-12-25 02:09:33 +0530

    changing server component to client

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,18 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import SignInButton from "./SignInButton";
-import { getAuthSession } from "@/lib/auth";
+// import { getAuthSession } from "@/lib/auth";
+
 import UserAccountNav from "./UserAccountNav";
 import { ThemeToggle } from "./ThemeToggle";
+import { getSession, useSession } from "next-auth/react";
 
 type Props = {};
 
-const Navbar = async (props: Props) => {
-  const session = await getAuthSession();
+const Navbar = (props: Props) => {
+  // const session =  getAuthSession();
+  const { data: session } = useSession();
   return (
     <nav className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
       <div className="flex items-center justify-center h-full gap-2 px-8 mx-auto sm:justify-between max-w-7xl">
